a3f2f30ffb42a321509d120909e5daaecf3cf5fe
André Lashley <andre.lashley@gmail.com>
2022-09-28 13:36:16 -0700

    Demonstrate separate memory stacks per thread

--- Program.cs
+++ Program.cs
@@ -7,16 +7,13 @@ namespace CSharpConcurrencyRecipes
     {
         static void Main(string[] args)
         {
-            Thread t = new Thread(WriteY); // kick off a new thread
-            t.Start(); // running WriteY()
-
-            // simultaneously do something on the main thread.
-            for (int i = 0; i < 1000; i++) Console.Write("x");
+            new Thread(Go).Start();
+            Go();
         }
 
-        static void WriteY()
+        static void Go()
         {
-            for (int i = 0; i < 1000; i++) Console.Write("y");
+            for (int cycles = 0; cycles < 5; cycles++) Console.Write("?");
         }
     }
 }
